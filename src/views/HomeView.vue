e9f56362116bfb309f6aac7ba6a160ed01f6605b
Farhan0511 <programingsukses.com>
2024-07-01 22:21:40 +0700

    added a little hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="home">
     <navbar />
-    <hero/>
+    <div class="mt-5 container">
+      <hero/>
+
+      <div class="best row">
+        <div class="col">
+          <h3>Best Food</h3>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right">Lihat semua</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
