1b825becbcf1bd676469c4a0a05a72af96a72be3
Farhan0511 <programingsukses.com>
2024-07-01 11:54:07 +0700

    menambahkan hero image

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="col mt-4">
-        <div class="row">
-            <h2 class="">"Menikmati Rasa Nusantara: Food Court yang Menghidangkan Kelezatan Lokal"</h2>
+     <div class="container">
+        <div class="row align-items-center justify-content-around">
+            <div class="col">
+                <h2>"Menikmati Rasa Nusantara: Tempat yang Menghidangkan Kelezatan Lokal"</h2>
+                <p>Ayo pilih makanan kesukaanmu!</p>
+                <button class="btn btn-lg btn-success">Pesan Sekarang<b-icon-arrow-right></b-icon-arrow-right></button>
+            </div>
+            <div class="col-md-5 text-center">
+                <img src="../assets/image/hero.png" alt="Hero Image" class="img-fluid" width="400px">
+            </div>
         </div>
-        <div class="row">
-            <img src="" alt="">
-        </div>
-        
     </div>
     
 </template>
