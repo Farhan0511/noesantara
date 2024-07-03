76efe45991ca876fd7d0007f820a6559559a99d5
Farhan0511 <programingsukses.com>
2024-07-03 10:58:20 +0700

    Add card product using Axios

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3 class="mt-5"> {{ product.nama }} </h3>
+</template>
+
+<script>
+export default {
+    name: 'CardProduct',
+    props: ['product']
+}
+</script>
+
+<style>
+
+</style>
