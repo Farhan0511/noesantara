21e00d5a638a38eb4228a9e1901711ba1045313d
Farhan0511 <programingsukses.com>
2024-07-10 22:45:49 +0700

    80% card components

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="card" style="width: 18rem">
-    <img :src=" '/assets/images/' +  product.gambar " class="card-img-top" alt="..." />
+  <div class="container card shadow card-costum" style="width: 18rem">
+    <img
+      :src="'/assets/images/' + product.gambar"
+      class="card-img-top"
+      alt="..."
+    />
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title">{{ product.nama }}</h5>
+      <p class="card-text">harga : Rp. {{ product.harga }}</p>
+      <a href="#" class="btn btn-success text-white btn-centered">
+        <b-icon-cart></b-icon-cart>
+        Pesan sekarang
+      </a>
     </div>
   </div>
 </template> 
